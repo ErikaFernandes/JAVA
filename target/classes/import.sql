29a074caee70a5264b9c4f5faa77f4b6d66196e1
Erika Tavares Fernandes <etavaresfernandes@gmail.com>
2018-10-18 02:14:25 -0300

    target

---
+++ target/classes/import.sql
@@ -0,0 +1,3 @@
+insert into challenge (name, description, status, type) values ('Practice English', 'Practice some content. Learn, watch, read or listen something in English','ACTIVE', 'FIXED');
+insert into challenge (name, description, status, type) values ('Practice Spring MVC', 'Keep Program your last project to learn more','ACTIVE', 'FIXED');
+insert into challenge (name, description, status, type) values ('Meditate', 'Take ten minute to meditate','ACTIVE', 'SURPRISE');
